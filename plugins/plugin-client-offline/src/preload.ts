3375e7b5dd580c2b77832680e673fa5d0d801c95
Nick Mitchell <nickm@us.ibm.com>
2022-01-09 12:04:29 -0500

    bumps

--- plugins/plugin-client-offline/src/preload.ts
+++ plugins/plugin-client-offline/src/preload.ts
@@ -1,1 +1,38 @@
-../../plugin-client-default/src/preload.ts
+/*
+ * Copyright 2020 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Capabilities } from '@kui-shell/core'
+
+/**
+ * Register the welcome notebook
+ *
+ */
+export default async () => {
+  if (!Capabilities.isHeadless() || Capabilities.inProxy()) {
+    const { notebookVFS } = await import('@kui-shell/plugin-core-support')
+    notebookVFS.cp(
+      undefined,
+      [
+        'plugin://plugin-client-common/notebooks/code-blocks.md',
+        'plugin://plugin-client-common/notebooks/expandable-section.md',
+        'plugin://plugin-client-common/notebooks/hints.md',
+        'plugin://plugin-client-common/notebooks/tabs.md',
+        'plugin://plugin-client-common/notebooks/playground.md'
+      ],
+      '/kui'
+    )
+  }
+}
